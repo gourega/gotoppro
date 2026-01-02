354810801af242ab25dcfb21820720a931ba3731
gourega <ourega.goble@canticthinkia.ci>
2026-01-02 12:33:58 +0000

    Fichier Wrangler

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./**/*.{js,ts,jsx,tsx}",
+    "./*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./contexts/**/*.{js,ts,jsx,tsx}",
+    "./services/**/*.{js,ts,jsx,tsx}",
+    "./constants/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
